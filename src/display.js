ad54b6ed132d3d2d1641846dfc5a22a80815e2d0
Nati Alex <natialex@Natis-MacBook-Air.local>
2021-10-08 14:57:36 -0400

    trying

---
+++ src/display.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./App.css";
+
+class Display extends Component {
+    render(){
+        return(
+            <div className="Display">
+                {this.props.data}
+            </div>
+        );
+    }
+}
+
+export default Display;
